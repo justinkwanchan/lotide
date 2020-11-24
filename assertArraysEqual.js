e9ee0c4916381bc11889f2e9bbace9865aa9224d
justinkwanchan <justinkwanchan@gmail.com>
2020-11-24 22:47:51 +0000

    Linted files

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -18,8 +18,8 @@ const assertArraysEqual = (array1, array2) => {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
--- without.js
+++ without.js
@@ -17,8 +17,3 @@ const eqArrays = (array1, array2) => {
   }
   return true;
 };
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
