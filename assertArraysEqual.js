8d2b32d0e230ef83f918525be9cf01674b859b0c
justinkwanchan <justinkwanchan@gmail.com>
2020-11-24 22:41:59 +0000

    Finished assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,8 +8,18 @@ const eqArrays = (array1, array2) => {
     }
   }
   return true;
-}
+};
 
 const assertArraysEqual = (array1, array2) => {
-  
-}
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
