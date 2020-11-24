116995c2625c8d528617053dbee692b9230b00a6
justinkwanchan <justinkwanchan@gmail.com>
2020-11-24 22:35:07 +0000

    Created assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,15 @@
+const eqArrays = (array1, array2) => {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (const i in array1) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = (array1, array2) => {
+  
+}
