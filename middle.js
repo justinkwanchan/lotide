dd6d9cc465654dc85a012a1a8d29a880cf103f41
justinkwanchan <justinkwanchan@gmail.com>
2020-11-30 18:11:23 +0000

    Refactored middle.js, exporting the function and moving the tests to its own file

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = (array1, array2) => {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (const i in array1) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = (array1, array2) => {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 const middle = inputArray => {
   const length = inputArray.length;
   if (length < 3) {
@@ -31,6 +11,4 @@ const middle = inputArray => {
   }
 };
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+module.exports = middle;
